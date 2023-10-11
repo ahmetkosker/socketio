88b946aed4e7ecaf520e365e13d6d23403f0867c
Ahmet <exarons@hotmail.com>
2023-10-11 12:42:58 +0300

    asdasd
    asdads

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+var cors = require("cors");
+const http = require("http");
+const app = express();
+const { Server } = require("socket.io");
+const axios = require("axios");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  let UID = socket.handshake.query['UID']
+  let newStatus = ""
+
+  console.log("here")
+
+  socket.on("ping", (status) => {
+    newStatus = status
+  });
+
+  socket.on("disconnect", () => {
+    if (newStatus !== "Görevde") {
+      axios
+        .post('https://statuschanger-lv73cppecq-ew.a.run.app', {
+          UID,
+          status: 'Dışarıda',
+        }).then(res => console.log(res.data.status))
+      console.log(`${socket.id} disconnected with UID ${UID}`);
+    }
+  });
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the chat server");
+});
+
+server.listen(3002, () => {
+  console.log("server is running");
+});
